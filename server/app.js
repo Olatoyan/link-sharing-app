8feae535f83dbbe9ed6a15af269870658d207ebe
Olatoyan <golatoyan@gmail.com>
2024-02-23 16:22:00 +0100

    added some security to the app

--- server/app.js
+++ server/app.js
@@ -5,6 +5,10 @@ const AppError = require("./utils/appError");
 const userRouter = require("./routes/userRoutes");
 const linkRouter = require("./routes/linkRoutes");
 const globalErrorHandler = require("./controllers/errorController");
+const helmet = require("helmet");
+const mongoSanitize = require("express-mongo-sanitize");
+const xss = require("xss-clean");
+const rateLimit = require("express-rate-limit");
 
 const app = express();
 
@@ -16,6 +20,20 @@ app.use(cors());
 app.options("*", cors());
 app.use(express.json({ limit: "50mb" }));
 app.use(express.urlencoded({ limit: "50mb", extended: true }));
+
+app.use(helmet());
+
+const limiter = rateLimit({
+  max: 100,
+  windowMs: 60 * 60 * 1000,
+  message: "Too many requests from this IP, please try again in an hour!",
+});
+app.use("/devlinks-api", limiter);
+
+// Data Sanitization against NoSQL query injection
+app.use(mongoSanitize());
+// Data Sanitization against XSS
+app.use(xss());
 
 app.use("/devlinks-api/v1/users", userRouter, linkRouter);
 
